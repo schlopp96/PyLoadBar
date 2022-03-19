83daa776d8d0e214387195050f37e27eb71d9954
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-03-18 20:28:55 -0500

    PLB v0.7.0 changes - Updated test file to match new param names.
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- PyLoadBar/tests/PyLoadBar_test.py
+++ PyLoadBar/tests/PyLoadBar_test.py
@@ -2,7 +2,7 @@ from PyLoadBar.load import load
 
 
 def test_loadA():
-    assert load(time=3, progressbar=False) is None
+    assert load(time=3, show_bar=False) is None
 
 
 def test_loadB():
@@ -11,8 +11,8 @@ def test_loadB():
 
 
 def test_loadC():
-    assert load("Exiting", "Finished!!", progressbar=False) is None
+    assert load("Exiting", "Finished!!", show_bar=False) is None
 
 
 def test_loadD():
-    assert load(4, 3463463.4, 3, False) is None
+    assert load(4, 3463463.4, time=3, show_bar=False) is None

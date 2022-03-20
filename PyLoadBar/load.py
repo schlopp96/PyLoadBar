55bed866b2ef367af257a10eff577b664d3e0111
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-03-19 20:13:20 -0500

    - Various changes to "./load.py".   - Renamed variables in `load()`.   -  Refactored into 2 separate methods.   - Modified unit tests to acommodate parameter changes.
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- PyLoadBar/load.py
+++ PyLoadBar/load.py
@@ -27,31 +27,32 @@ logger.addHandler(fh)
 
 def load(msg_loading: str = 'Loading',
          msg_complete: str = 'Done!',
-         label: str = None,
          time: int = 5,
-         show_bar: bool = True) -> None:
+         label: str = None,
+         enable_display: bool = True) -> None:
     """Breifly pause program while optionally displaying a customizable loading message, alongside an optional progress bar, to the console (stdout).
 
-    - User may set custom strings through the parameters `msg_loading` and `msg_complete`.
+    - User may set custom loading/completion strings through the parameters `msg_loading` and `msg_complete`.
     - Set the length of pause in seconds using the `time` parameter.
         - Every increment of units = 0.1 seconds.
-    - Displaying a progress bar is activated by default, but may be disables using the `progressbar` parameter.
+    - Displaying a progress bar is activated by default, but may be disabled using the `show_loadbar` parameter.
+    -
     - Examples:
-        - `>>> load(time=5, progressbar=True)` will take around 0.5 seconds to fill the progress bar.
-        - `>>> load(time=20, progressbar=False)` will take around 2 seconds for `msg_complete` to display, following `msg_loading`.
+        - `>>> load(time=5, enable_display=True)` will take around 0.5 seconds to fill the progress bar.
+        - `>>> load(time=20, enable_display=False)` will take around 2 seconds for `msg_complete` to display, following `msg_loading`.
 
     Parameters:
         :param msg_loading: message to display during loading process, defaults to `"Loading"`.
         :type msg_loading: str, optional
         :param msg_complete: message to display upon load completion, defaults to `"Done!"`.
         :type msg_complete: str, optional
-        :param label: label of progress bar.
-        :type label: str, optional
-        :param time: time in seconds for progress bar to reach completion, defaults to `5`.
+        :param time: time for progress bar to reach completion in units of 0.1 seconds, defaults to `5`.
         :type time: int, optional
-        :param show_bar: whether to display progress bar during loading process, defaults to `True`
-        :type show_bar: bool, optional
-        :return: loading sequence accompanied by an explanatory message to the user, and optional progress bar.
+        :param label: label of progress bar, defaults to `None`.
+        :type label: str, optional
+        :param enable_display: whether to display progress bar during loading process, defaults to `True`
+        :type enable_display: bool, optional
+        :return: loading sequence accompanied by an explanatory message to the user, optionally visualized using a progress bar.
         :rtype: None
     """
 
@@ -63,30 +64,46 @@ def load(msg_loading: str = 'Loading',
             msg_complete = msg_complete
 
         #$ Return load sequence:
-        if show_bar:
-            logger.info(
-                f'Begin loading sequence using progress bar...\n-> Loading Message: "{msg_loading}"\n'
-            )
-            print(f'{msg_loading}...\n')
-            for time in tqdm.trange(time, desc=label):
-                s(0.1)
-                time -= 1
+        if enable_display:
+            return loadseq_enabled(msg_loading, msg_complete, time, label)
         else:
-            logger.info(
-                f'Begin loading sequence NOT using progress bar...\n-> Loading Message: "{msg_complete}"\n'
-            )
-            print(f'{msg_loading}', end='')
-            for time in range(time):
-                print('.', end='')
-                s(0.1)
-                time -= 1
-        logger.info(
-            f'Completed loading process!\n-> Completed Message: "{msg_complete}"\n-> Time to completion: {(time * 0.1)+0.2} seconds.\n'
-        )
-        print(f'\n{msg_complete}')
-        s(0.5)
-    except ValueError as VE:
-        logger.exception(f'Exception occurred:\n===>{VE}')
+            return loadseq_disabled(msg_loading, msg_complete, time)
+    except ValueError as ve:
+        logger.exception(f'{ve}')
+
+
+def loadseq_disabled(msg_loading, msg_complete, time: int = 5) -> None:
+    logger.info(
+        f'Begin loading sequence NOT using progress bar...\n-> Loading Message: "{msg_loading}"\n'
+    )
+    print(f'{msg_loading}', end='')
+    for time in range(time):
+        print('.', end='')
+        s(0.1)
+        time -= 1
+    logger.info(
+        f'Completed loading process!\n-> Completed Message: "{msg_complete}"\n-> Time to completion: {(time * 0.1)+0.2} seconds.\n'
+    )
+    print(f'\n\n{msg_complete}')
+    s(0.5)
+
+
+def loadseq_enabled(msg_loading,
+                    msg_complete,
+                    time: int = 5,
+                    label: str = None) -> None:
+    logger.info(
+        f'Begin loading sequence using progress bar...\n-> Loading Message: "{msg_loading}"\n'
+    )
+    print(f'{msg_loading}...\n')
+    for time in tqdm.trange(time, desc=label):
+        s(0.1)
+        time -= 1
+    logger.info(
+        f'Completed loading process!\n-> Completed Message: "{msg_complete}"\n-> Time to completion: {(time * 0.1)+0.2} seconds.\n'
+    )
+    print(f'\n{msg_complete}')
+    s(0.5)
 
 
 if __name__ == '__main__':

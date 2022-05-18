52acfbaeb9a40fdf9c9baf2d2114ad5a693f1c93
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-05-17 21:34:45 -0500

    - Improved method inline-docs. - Changed "if-else" statement to "match-case" in `load` method.
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- PyLoadBar/main.py
+++ PyLoadBar/main.py
@@ -8,17 +8,38 @@ __version__ = '0.0.8'
 
 
 class PyLoadBar:
-    """Breifly pause program while optionally displaying a customizable loading message, alongside an optional progress bar, to the console (stdout).
+    """Create simple loading sequences with ability to customize start/finish messages, toggle visual progress meter, and set time to completion."""
 
-        - User may set custom loading/completion strings through the parameters `msg_loading` and `msg_complete`.
-        - Set the length of pause in seconds using the `time` parameter.
-            - Every increment of units = 0.1 seconds.
-        - Displaying a progress bar is activated by default, but may be disabled using the `enable_display` parameter.
-        - A custom label may be set using the `label` parameter.
+    def __init__(self):
+        pass
 
-        - Examples:
-            - `>>> bar = PyLoadBar(time=5, label='Processing', enable_display=True)` <-- Will take 0.5 seconds to fill the progress bar.
-            - `>>> bar = PyLoadBar(time=20, enable_display=False)` <-- Will take 2 seconds for `msg_complete` to display following `msg_loading`.
+    def load(self,
+                 msg_loading: str = 'Loading',
+                 msg_complete: str = 'Done!',
+                 time: int = 5,
+                 label: str = None,
+                 enable_display: bool = True) -> None:
+        """
+        Start a loading sequence which includes start/completion messages, and an optional progress meter.
+
+        - User may set custom start/completion messages using the parameters `msg_loading: str` and `msg_complete: str` respectively.
+        - Set the sequence length in milliseconds using the `time: int` parameter.
+            - Each unit of time = 0.1 seconds.
+        - Display of the progress meter is activated by default, but may be disabled using the `enable_display` parameter.
+        - The progress meter may be labeled using the `label: str` parameter (set to `None` by default).
+            - Note that `enable_display` must be set to `True` for this to take effect.
+
+        Examples:
+
+            - Load sequence WITH progress meter (default):
+
+                >>> bar_ON = PyLoadBar() # < Create class instance.
+                >>> bar_ON.load(msg_loading='Printing', msg_complete='Complete!', time=50) # <- Will take 5 seconds to complete loading sequence.
+
+            - Load sequence WITHOUT progress meter:
+
+                >>> bar_OFF = PyLoadBar() # < Create class instance.
+                >>> bar_OFF.load('Processing Information', 'Finished!', time=20, enable_display=False) # <- Will take 2 seconds to complete loading sequence.
 
         ---
 
@@ -26,41 +47,29 @@ class PyLoadBar:
         :type msg_loading: str, optional
         :param msg_complete: message to display upon load completion, defaults to `"Done!"`.
         :type msg_complete: str, optional
-        :param time: time for progress bar to reach completion in units of 0.1 seconds, defaults to `5`.
+        :param time: time for loading sequence to complete measured in units of 0.1 seconds, defaults to `5`.
         :type time: int, optional
-        :param label: label of progress bar, defaults to `None`.
+        :param label: label preceding progress bar, defaults to `None`.
         :type label: str, optional
-        :param enable_display: whether to display progress bar during loading process, defaults to `True`
+        :param enable_display: toggle display of progress bar during loading process, defaults to `True`.
         :type enable_display: bool, optional
-        :return: loading sequence accompanied by an explanatory message to the user, optionally visualized using a progress bar.
+        :return: loading sequence with optional visualization using progress meter.
         :rtype: None
         """
 
-    def __init__(self,
-                 msg_loading: str = 'Loading',
-                 msg_complete: str = 'Done!',
-                 time: int = 5,
-                 label: str = None,
-                 enable_display: bool = True) -> None:
+        match enable_display:
+            case True:
+                print(f'{msg_loading}...\n')
+                for iter in tqdm.trange(time, desc=label):
+                    s(0.1)
+                    iter -= 1
+                print(f'\n{msg_complete}')
 
-        self.msg_loading = msg_loading
-        self.msg_complete = msg_complete
-        self.time = time
-        self.label = label
-        self.enable_display = enable_display
-
-    def load(self) -> None:
-        if self.enable_display:
-            print(f'{self.msg_loading}...\n')
-            for time in tqdm.trange(self.time, desc=self.label):
-                s(0.1)
-                time -= 1
-            print(f'\n{self.msg_complete}')
-        else:
-            print(f'{self.msg_loading}', end='')
-            for time in range(self.time):
-                print('.', end='')
-                s(0.1)
-                time -= 1
-            print(f'\n\n{self.msg_complete}')
-        s(0.5)
+            case False:
+                print(f'{msg_loading}', end='')
+                for iter in range(time):
+                    print('.', end='')
+                    s(0.1)
+                    iter -= 1
+                print(f'\n\n{msg_complete}')
+        s(0.5) # Add a small delay to allow user to see completion message.

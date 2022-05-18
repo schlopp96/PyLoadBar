142ee8ed96658767e7d335686d9cfb9f77d2ae84
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-05-17 22:20:09 -0500

    - Fixed tests
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- PyLoadBar/tests/PyLoadBar_test.py
+++ PyLoadBar/tests/PyLoadBar_test.py
@@ -2,21 +2,21 @@ from PyLoadBar.main import PyLoadBar
 
 
 def test_loadA():
-    bar = PyLoadBar(time=1, enable_display=False)
-    assert bar.load() is None
+    bar = PyLoadBar()
+    assert bar.load(time=1, enable_display=False) is None
 
 
 def test_loadB():
-    bar = PyLoadBar("Taking sweet time",
-                "Finally...\n\nTook long enough.")
-    assert bar.load() is None
+    bar = PyLoadBar()
+    assert bar.load("Taking sweet time",
+                    "Finally...\n\nTook long enough.") is None
 
 
 def test_loadC():
-    bar = PyLoadBar('Exiting', 'Finished!!', enable_display=False)
-    assert bar.load() is None
+    bar = PyLoadBar()
+    assert bar.load('Exiting', 'Finished!!', enable_display=False) is None
 
 
 def test_loadD():
-    bar = PyLoadBar(4, 3463463.4, time=3, enable_display=False)
-    assert bar.load() is None
+    bar = PyLoadBar()
+    assert bar.load(4, 3463463.4, time=3, enable_display=False) is None

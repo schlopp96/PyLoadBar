98020bb9ab6f15a8d587be90a0e32bbefa02a950
schlopp96 <71921821+schlopp96@users.noreply.github.com>
2022-07-18 02:51:49 -0500

    v0.1.1 Hotfix Update
    ---
    
    - Removed unnessecary command to set cwd to module directory.
    
    Signed-off-by: schlopp96 <71921821+schlopp96@users.noreply.github.com>

--- PyLoadBar/main.py
+++ PyLoadBar/main.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import sys
-from os import chdir
 from os.path import dirname
 from random import uniform
 from time import sleep
@@ -11,9 +10,7 @@ import tqdm
 sys.path.insert(0, dirname(
     dirname(__file__)))  # Ensure module can be found by Python.
 
-chdir(dirname(__file__))  # Change working directory to main module.
-
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 
 
 class PyLoadBar:
